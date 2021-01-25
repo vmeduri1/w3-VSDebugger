ba8d5d861ab0349856632ac53e4d6cdfa3ad124f
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-01-25 16:43:45 -0600

    Second commit

--- problems/03-water-tracker.js
+++ problems/03-water-tracker.js
@@ -8,29 +8,38 @@ For example, in this week, [0, 0, 3, 1, 0, 4, 0], each day represents how many
 cups of water you drank that day. In this example, there were only 3 days where
 you drank at least one cup of water.
 
-A calendar is represented by multiple weeks, 
+A calendar is represented by multiple weeks,
 [[0, 0, 3, 1, 0, 4, 0], [1, 2, 1, 2, 1, 3, 1]].
 
 If you drank water for at least 4 days of water for every week in the calendar,
 then return true. Otherwise, return false.
 
 */
-
+debugger
 function adequateWaterTracker(calendar) {
-  let noWater = 0;
-  let water = 0;
+  debugger
+  let weeksCount = 0;
   for (let i = 0; i < calendar.length; i++) {
-    const week = calendar[i];
-    for (let j = 0; j < week.length - 1; j++) {
-      const day = calendar[j];
-      if (day === 0) {
-        noWater++;
-      } else {
-        water++;
+    debugger
+    let daysCount = 0;
+    for (let j = 0; j < calendar[i].length; j++) {
+      debugger
+      let day = calendar[i][j];
+      if (day > 0) {
+        debugger
+        daysCount++;
+        //console.log(daysCount);
       }
     }
+    if (daysCount >= 4) {
+      debugger
+      weeksCount++;
+      //console.log(weeksCount);
+    }
   }
-  return noWater < water;
+
+  debugger
+  return weeksCount >= calendar.length;
 }
 
 const calendar1 = [
@@ -53,4 +62,3 @@ const calendar3 = [
 ];
 
 console.log(adequateWaterTracker(calendar3)); // true
-
